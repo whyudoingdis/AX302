8c68ae8bd0cbd2c370127f62cf28e16df67f8558
whyudoingdis <jamiehype2008@gmail.com>
2019-03-02 17:45:19 +0800

    lesson 8

--- Phaser/app.js
+++ Phaser/app.js
@@ -31,7 +31,7 @@ ledge2 = platforms.create(-100,250,'platform');
 
 	var style = {font: "bold 32px Arial", fill:"#fff"};
 	scorelabel = game.add.text(200,360,"Score:", style);
-	scoretext = game.add.text(250,400,score, style);
+	scoretext = game.add.text(200,400,score, style);
 	
 	livelabel = game.add.text(300,560,"Lives:", style);
 	livetext = game.add.text(420,560,lives, style);
@@ -39,4 +39,58 @@ ledge2 = platforms.create(-100,250,'platform');
 	livelabel.setShadow(3,3,'rgba(0,0,0,0.5)',2);
 }
 
-function update(){}
+player = game.add.sprite(10,300,'dude');
+
+player.animations.add('left',[0,1,2,3],10,true);
+player.animations.add('right',[5,6,7,8],10,true);
+
+player.body.gravity.y = 300;
+player.body.bounce.y = 0.2;
+player.body.collideWorldBounds = true;
+
+enemy.body.gravity.y = 300;
+enemy.body.bounce.y = 0.2;
+enemy.body.collideWorldBounds = true;
+
+enemy = game.add.sprite(10,300,'enemy');
+
+enemy.animations.add('left',[0,1],10,true);
+enemy.animations.add('right',[2,3],10,true);
+
+stars = game.add.physicsGroup();
+stars.enableBody = true;
+for(var i = 0; i < 12	; i++){
+	var star = stars.create( i*70 ,  0, 'star' )
+	stae.body.gravity.y = 200;
+	star.body.bounce.y = Math.random()*0.2 + 0.7
+
+
+wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+dKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
+aKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
+}
+function update(){
+game.physics.arcade.collide(player, platforms);
+game.physics.arcade.collide(stars, platforms);
+game.physics.arcade.collide(enemy, platforms);
+
+
+player.body.velocity.x = 0;
+
+
+if(aKey.isDown)
+player.body.velocity.x = -150;
+player.animations.play("left");
+
+} else  
+if(dKey.isDown)
+player.body.velocity.x = +150;
+player.animations.play("right");
+
+else {
+	player.animations.stop();
+	player.frame = 4;
+}
+if wKey.isDown && player.body.touching.down {
+	player.body.velocity.y = -300;
+}
