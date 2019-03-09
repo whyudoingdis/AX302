36a7b49476b1e7f997b10ba20ba5cb3798b570fe
whyudoingdis <jamiehype2008@gmail.com>
2019-03-09 17:54:35 +0800

    lesson 8

--- Phaser/app.js
+++ Phaser/app.js
@@ -33,11 +33,11 @@ ledge2 = platforms.create(-100,250,'platform');
 	scorelabel = game.add.text(200,360,"Score:", style);
 	scoretext = game.add.text(200,400,score, style);
 	
-	livelabel = game.add.text(300,560,"Lives:", style);
-	livetext = game.add.text(420,560,lives, style);
+	liveslabel = game.add.text(10,10,"Lives:", style);
+	livestext = game.add.text(120,10,lives, style);
 
-	livelabel.setShadow(3,3,'rgba(0,0,0,0.5)',2);
-}
+	liveslabel.setShadow(3,3,'rgba(0,0,0,0.5)',2);
+
 
 player = game.add.sprite(10,300,'dude');
 
@@ -48,12 +48,13 @@ player.body.gravity.y = 300;
 player.body.bounce.y = 0.2;
 player.body.collideWorldBounds = true;
 
+enemy = game.add.sprite(10,300,'baddie');
 game.physics.arcade.enable(enemy)
 enemy.body.gravity.y = 300;
 enemy.body.bounce.y = 0.2;
 enemy.body.collideWorldBounds = true;
 
-enemy = game.add.sprite(10,300,'enemy');
+
 
 enemy.animations.add('left',[0,1],10,true);
 enemy.animations.add('right',[2,3],10,true);
@@ -62,10 +63,10 @@ stars = game.add.physicsGroup();
 stars.enableBody = true;
 for(var i = 0; i < 12	; i++){
 	var star = stars.create( i*70 ,  0, 'star' )
-	stae.body.gravity.y = 200;
+	star.body.gravity.y = 200;
 	star.body.bounce.y = Math.random()*0.2 + 0.7
 
-
+}
 wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
 dKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
 aKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
@@ -83,7 +84,7 @@ if(aKey.isDown){
 player.body.velocity.x = -150;
 player.animations.play("left");
 
-} else if(dKey.isDown)
+} else if(dKey.isDown){
 player.body.velocity.x = +150;
 player.animations.play("right");
 }
@@ -91,6 +92,59 @@ else {
 	player.animations.stop();
 	player.frame = 4;
 }
-if wKey.isDown && player.body.touching.down {
+if( wKey.isDown && player.body.touching.down ){
 	player.body.velocity.y = -300;
+
 }
+game.physics.arcade.overlap(player,stars,collectStar);
+game.physics.arcade.overlap(player, enemy, loseLife);
+
+moveEnemy();
+
+if(lives < 1){
+	endGame();
+}
+
+
+
+}
+
+
+function collectStar(player, star){
+	score++;
+	scoretext.setText(score);
+
+	star.kill();
+	star.reset(Math.random()*750, 0);
+}
+
+
+
+function loseLife(player, life){
+	lives--;
+	livestext.setText(lives);
+	player.kill();
+	player.reset(10,20);
+}
+function moveEnemy(){
+	if(enemy.x > 700){
+		enemy.animations.play('left');
+		enemy.body.velocity.x = -120;
+
+	} else if(enemy.x < 11){
+		enemy.animations.play('right');
+		enemy.body.velocity.x = 120;
+
+	}
+}
+function endGame(){
+	player.kill();
+	liveslabel.visible = false;
+	livestext.visible = false;
+	scoretext.visible = false;
+	scorelabel.text = "GAME OVER! You scored" + score;
+}
+
+
+
+
